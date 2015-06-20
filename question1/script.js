f94a3fe06a9dc6c2923920bc73b744f91141f44a
Molly Kent <kent.molly@gmail.com>
2015-06-20 16:15:52 -0700

    trial editable text fuction

--- question1/script.js
+++ question1/script.js
@@ -1,7 +1,48 @@
 'use strict';
 
 $(function() {
-  $('li').click(function() {
-    $(this).next('.details').toggleClass('hidden');
+  $('p').click(function() {
+    $('div.details').toggleClass('hidden');
   })
+  $('button.edit').click(function() {
+    $(this).parent('.item').attr('class','urgent');
+  })
+
+
+
+
+var replaceWith = $('<input name="temp" type="text" />'),
+    connectWith = $('input[name="hiddenField"]');
+
+
+
+$.fn.inlineEdit = function(replaceWith, connectWith) {
+
+    $(this).hover(function() {
+        $(this).addClass('hover');
+    }, function() {
+        $(this).removeClass('hover');
+    });
+
+    $(this).click(function() {
+
+        var elem = $(this);
+
+        elem.hide();
+        elem.after(replaceWith);
+        replaceWith.focus();
+
+        replaceWith.blur(function() {
+
+            if ($(this).val() != "") {
+                connectWith.val($(this).val()).change();
+                elem.text($(this).val());
+            }
+
+            $(this).remove();
+            elem.show();
+        });
+    });
+};
+$('p#trial').inlineEdit(replaceWith, connectWith);
 });
