94f02faa7d505d987738e37ed063115d105ce97f
Molly Kent <kent.molly@gmail.com>
2015-06-22 11:26:32 -0700

    initial commit

---
+++ question3/script.js
@@ -0,0 +1,20 @@
+'use strict';
+$(function() {
+
+  function Library() {
+    this.shelves = [];
+  };
+
+  function Shelf() {
+    this.books = [];
+    var addBook = new Book();
+    var removeBook = function(bookTitle) {
+      //reference the book
+    }
+  }
+
+  function Book() {
+    this.author = author;
+    this.bookTitle = bookTitle;
+  }
+});
